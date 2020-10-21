f770f52f999fab3474570a5f588a4c76c12bcd3c
Stacy Potten <stacylynnaz@gmail.com>
2020-10-21 12:08:45 -0600

    Add test to for adding a new reservation

--- src/App/App.test.js
+++ src/App/App.test.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { render, screen, fireEvent } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import '@testing-library/jest-dom';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+describe('App', () => {
+  it('should add a new reservation', () => {
+    render(<App />);
+    userEvent.type(screen.getByPlaceholderText('Name'), 'Stacy');
+    userEvent.type(screen.getByPlaceholderText('Date'), '10/22');
+    userEvent.type(screen.getByPlaceholderText('Time'), '7:00');
+    userEvent.type(screen.getByPlaceholderText('Number'), '10');
+    userEvent.click(screen.getByRole('button', { name: 'Make a Reservation'}));
+    expect(screen.getByRole('heading', { name: 'Stacy'})).toBeInTheDocument();
+  })
+})
