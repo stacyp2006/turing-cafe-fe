f1feafe7b80822d3a0357cc7b8f8e18653d7f9f8
Stacy Potten <stacylynnaz@gmail.com>
2020-10-21 10:59:00 -0600

    Create basic structure of form component

--- src/Form/Form.js
+++ src/Form/Form.js
@@ -1,4 +1,55 @@
 import React, { Component } from 'react';
 import './Form.css';
 
-export default Form; 
+class Form extends Component {
+  constructor() {
+    super();
+    this.state = {
+
+    }
+  }
+
+  render() {
+    return(
+      <div className="make-reservation">
+        <form className="reservation-form">
+          <input
+              className="name-input"
+              type='text'
+              placeholder='Name'
+              name='name'
+              value={this.state.name}
+              onChange={this.updateValue}
+          />
+          <input
+              className="date-input"
+              type='text'
+              placeholder='Date'
+              name='date'
+              value={this.state.date}
+              onChange={this.updateValue}
+          />
+          <input
+              className="time-input"
+              type='text'
+              placeholder='Time'
+              name='time'
+              value={this.state.time}
+              onChange={this.updateValue}
+          />
+          <input
+              className="number-input"
+              type='number'
+              min='1'
+              placeholder='Number'
+              name='number'
+              value={this.state.number}
+              onChange={this.updateValue}
+          />
+          <button className="res-btn">Make a Reservation</button>
+        </form>
+      </div>
+    )
+  }
+}
+export default Form;
