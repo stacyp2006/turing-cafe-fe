a33f787b2b8a055aadb3d196c629ffb30681337c
Stacy Potten <stacylynnaz@gmail.com>
2020-10-21 10:08:56 -0600

    Add componentDidMount with fetch

--- src/App/App.js
+++ src/App/App.js
@@ -1,7 +1,20 @@
 import React, { Component } from 'react';
+import ResContainer from '../ResContainer/ResContainer.js';
+import { getReservations } from '../apiCalls.js';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      reservations: [];
+    }
+  }
+  componentDidMount() {
+    getReservations()
+    .then(data => this.setState({reservations: data}))
+    .catch(error => console.log('get error'))
+  }
   render() {
     return (
       <div className="App">
@@ -9,7 +22,6 @@ class App extends Component {
         <div className='resy-form'>
         </div>
         <div className='resy-container'>
-
         </div>
       </div>
     )
